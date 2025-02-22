1a5bcfd019f46e289ff0068431c4f2315bdba51d
frozenrx1 <frozenrx33@icloud.com>
2025-02-22 15:45:04 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Artem Zarkov");
+
+        int income = 15000;
+        int spending = 13400;
+
+        System.out.println("Итого (руб): \n" + (income - spending));
+
+    }
+}
